7016464223a57ad128fd800d9ad947d14210c896
mkangelo <mikequi000@gmail.com>
2024-09-08 16:13:44 -0600

    Youtube code

--- Templates/template.cpp
+++ Templates/template.cpp
@@ -1,14 +1,23 @@
+// Author: Angel Quintana
+
 #include <bits/stdc++.h>
+
 using namespace std;
 
-void run_case(){
-	
+void solve(){
+
 }
 
 int main(){
-    ios_base::sync_with_stdio(0); cin.tie(0);
-	int tt; cin >> tt;
-	for(int i = 0; i<tt; ++i){
-		run_case();
-	}
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0);
+
+    int tt;
+    cin >> tt;
+    //tt = 1;
+    for(int i=0; i<tt; ++i){
+        solve();
+    }
+
+    return 0;
 }
